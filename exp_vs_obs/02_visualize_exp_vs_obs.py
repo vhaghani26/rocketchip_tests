7ebed963ce3358e0f57312c195e57f9487f6217d
vhaghani26 <vhaghani@ucdavis.edu>
2023-08-03 15:58:08 -0700

    start working on heatmap

--- exp_vs_obs/02_visualize_exp_vs_obs.py
+++ exp_vs_obs/02_visualize_exp_vs_obs.py
@@ -36,10 +36,49 @@ plt.show()
 plt.savefig('02_tables_and_figures/total_distribution_of_observed_peaks.pdf')
 '''
 
-##########################
-## Table of Proportions ##
-##########################
+##############
+## Heatmaps ##
+##############
 
+# Filter rows so peak caller is unique for peak type and endedness groups
+heatmap_df = df.groupby(["Endedness", "Peak_Type"]).filter(lambda x: x["Peak_Caller"].nunique() == 1)
+
+# Generate a heatmap for each unique combination of peak type, endedness, and peak caller
+for (readtype, peaktype, peakcaller), data in heatmap_df.groupby(["Endedness", "Peak_Type", "Peak_Caller"]):
+    
+    # Plot size
+    plt.figure(figsize=(10, 6))
+    
+    # Create a matrix for the heatmap
+    pivot_df = data.pivot(index = "Deduplicator", columns = "Aligner", values = "Observed_Peaks")
+    sns.heatmap(pivot_df, cmap = "coolwarm", annot = True, fmt = ".2f", cbar_kws = {'label': 'Observed Peaks'})
+    
+    # Format naming
+    if peakcaller == "macs3": 
+        peakcaller_name = "MACS3"
+    elif peakcaller == "genrich":
+        peakcaller_name = "Genrich"
+    elif peakcaller == "cisgenome":
+        peakcaller_name = "CisGenome"
+    elif peakcaller == "pepr":
+        peakcaller_name = "PePr"
+    
+    # Configure titles and axis labels
+    plt.title(f'Number of Peaks for {readtype.title()}-End Data with {peaktype.title()} Peaks using {peakcaller_name}')
+    plt.xlabel("Aligner")
+    plt.ylabel("Deduplicator")
+    
+    # Show figure
+    plt.show()
+    
+    # Save figure
+    plt.savefig(f'02_tables_and_figures/heatmap_{readtype}_{peaktype}_{peakcaller}.pdf')
+
+#########################
+## Table of Statistics ##
+#########################
+
+'''
 # Condense tests for each condition and calculate basic stats
 proportions_per_condition = df.groupby(["Endedness", "Peak_Type", "Aligner", "Peak_Caller", "Deduplicator", "Control"]).agg(
     Min_Observed_Peaks = ("Observed_Peaks", "min"),
@@ -77,43 +116,4 @@ merged_df = merged_df.sort_values(by="Percent_Accuracy", ascending = False)
 
 # Save to CSV
 merged_df.to_csv("02_tables_and_figures/02_expected_vs_observed_results.csv", index = False)
-
-##############
-## Heatmaps ##
-##############
-
-'''
-# Filter rows so peak caller is unique for peak type and endedness groups
-heatmap_df = df.groupby(["Endedness", "Peak_Type"]).filter(lambda x: x["Peak_Caller"].nunique() == 1)
-
-# Generate a heatmap for each unique combination of peak type, endedness, and peak caller
-for (readtype, peaktype, peakcaller), data in heatmap_df.groupby(["Endedness", "Peak_Type", "Peak_Caller"]):
-    
-    # Plot size
-    plt.figure(figsize=(10, 6))
-    
-    # Create a matrix for the heatmap
-    pivot_df = data.pivot(index="Deduplicator", columns="Aligner", values="Observed_Peaks")
-    sns.heatmap(pivot_df, cmap="coolwarm", annot=True, fmt=".2f", cbar_kws={'label': 'Observed Peaks'})
-    
-    # Format naming
-    if peakcaller == "macs3":
-        peakcaller_name = "MACS3"
-    elif peakcaller == "genrich":
-        peakcaller_name = "Genrich"
-    elif peakcaller == "cisgenome":
-        peakcaller_name = "CisGenome"
-    elif peakcaller == "pepr":
-        peakcaller_name = "PePr"
-    
-    # Configure titles and axis labels
-    plt.title(f'Number of Peaks for {readtype.title()}-End Data with {peaktype.title()} Peaks using {peakcaller}')
-    plt.xlabel("Aligner")
-    plt.ylabel("Deduplicator")
-    
-    # Show figure
-    plt.show()
-    
-    # Save figure
-    plt.savefig(f'02_tables_and_figures/heatmap_{readtype}_{peaktype}_{peakcaller}.pdf')
 '''
