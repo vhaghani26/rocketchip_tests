7606e09f63f9a95b2fda67030d019296549bcadb
vhaghani26 <vhaghani@ucdavis.edu>
2024-09-17 10:16:08 -0700

    fix illegal combos

--- chip_seq/deep_dive_yaml.py
+++ chip_seq/deep_dive_yaml.py
@@ -1,3 +1,9 @@
+#!/bin/bash python3 
+
+'''
+python3 deep_dive_yaml.py
+'''
+
 import yaml
 
 # Variables
@@ -9,10 +15,10 @@ peakcallers = ["macs3", "cisgenome", "genrich", "pepr"]
 deduplicators = ["samtools", "no_deduplication", "sambamba", "picard"]
 num_tests = 100
 
-# List to store sample names
+# Store sample names
 samples = []
 
-# Loop to generate sample names
+# Generate sample names, skipping invalid combinations
 for control in controltypes:
     for project in projects:
         for peaktype in peaktypes:
@@ -20,6 +26,12 @@ for control in controltypes:
                 for peakcaller in peakcallers:
                     for deduplicator in deduplicators:
                         for i in range(1, num_tests + 1):
+
+                            # Skip illegal combinations
+                            if control == "no_control" and (peakcaller == "cisgenome" or peakcaller == "pepr"):
+                                continue
+
+                            # Generate valid combinations
                             sample_name = f"{project}_{control}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test{i}"
                             samples.append(sample_name)
 
@@ -32,5 +44,4 @@ data = {
 with open('snakefiles.yaml', 'w') as file:
     yaml.dump(data, file, default_flow_style=False)
 
-print("snakefiles.yaml has been created")
-
+print("snakefiles.yaml has been created with valid combinations!")
